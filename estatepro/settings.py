342378f67d9c9b0a7c2b17b94047e6db0766b5d4
harikrishnamanoj79-alt <harikrishnamanoj79@gmail.com>
2026-03-05 14:49:10 +0530

    fix whitenoise static

--- estatepro/settings.py
+++ estatepro/settings.py
@@ -140,7 +140,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.StaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"
     },
 }
 
