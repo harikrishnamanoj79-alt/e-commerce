187972b1a5d6806ba413ef46a5bad009407e6e92
harikrishnamanoj79-alt <harikrishnamanoj79@gmail.com>
2026-03-10 12:08:27 +0530

    add num2words dependency

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
+set -o errexit
 
 pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
-python manage.py migrate
+python manage.py migrate --noinput
 
-python manage.py loaddata data.json 
+# create admin user automatically
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("admin", "admin@example.com", "admin123")
+END
